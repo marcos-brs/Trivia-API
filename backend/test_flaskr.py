49515c689e9226d125c79addd45aa7d3480d98e2
Marcos Santana <brmarcossantanabr@gmail.com>
2020-07-26 08:21:08 -0300

    Update database config

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,12 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_user = "postgres"
+        self.database_password = "docker"
+        self.database_host = "localhost"
+        self.dabase_port = 5432
+        self.database_path = "postgresql://{}:{}@{}:{}/{}".format(
+            self.database_user, self.database_password, self.database_host, self.dabase_port, self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,7 +29,7 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
